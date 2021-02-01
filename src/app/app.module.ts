8dab486ae760493c08be5d17a1d7db55fdb90848
NOBIPROG <ispannorbert@gmail.com>
2021-02-01 09:45:31 +0100

    Hallgatói

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StudentRowComponent } from './components/student-row/student-row.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    HeaderComponent,
+    StudentRowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
