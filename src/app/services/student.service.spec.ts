e6b89e662d5a6218d7a2f01d306a3f5bbb5c9039
NOBIPROG <ispannorbert@gmail.com>
2021-02-23 10:18:21 +0100

    finish

--- src/app/services/student.service.spec.ts
+++ src/app/services/student.service.spec.ts
@@ -1,16 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { StudentService } from './student.service';
+import {StudentService} from './student.service';
+import {HttpClient} from '@angular/common/http';
+import {StudentResponse} from '../interfaces/student-response';
+import {Observable, of} from 'rxjs';
+
 
 describe('StudentService', () => {
   let service: StudentService;
-
+  let spy: jasmine.SpyObj<HttpClient>;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StudentService);
+    spy = jasmine.createSpyObj('HttpClient', ['get', 'post']);
+    // service = TestBed.inject(StudentService);
+    service = new StudentService({} as HttpClient);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  it ('should get a student from server', function(){
+    const studentResponse: StudentResponse = {
+      success: true,
+      students: [student]
+    };
+    const responsObs: Observable<StudentResponse> = of(studentResponse);
+    spy.get.and.returnValue(responsObs);
+    service.getStudents(stu)
   });
 });
